91b269d0b6eb1523cb754bb188cab42c9fdda953
Richard <richard.santarinareyes@medieinstitutet.se>
2021-03-04 18:39:46 +0100

    error if email is already taken

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -2,7 +2,7 @@
     class Users extends Controller {
         public function __construct()
         {
-            
+            $this->userModel = $this->model("User");
         }
 
         public function register(){
@@ -27,6 +27,10 @@
 
                 if(empty($data["email"])){
                     $data["email_err"] = "Please enter email";
+                } else {
+                    if($this->userModel->findUserByEmail($data["email"])){
+                        $data["email_err"] = "Email is already registered";
+                    }
                 }
 
                 if(empty($data["password"])){
